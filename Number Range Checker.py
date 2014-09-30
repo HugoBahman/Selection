c10e96c81704ba9dce8c888331ffe62b80b3ede7
Hugo Bahman <41408@longroad.ac.uk>
2014-09-30 12:47:02 +0100

    Checks to see if a number is between 21 and 29

---
+++ Number Range Checker.py
@@ -0,0 +1,11 @@
+#Hugo
+#Number Range Checker
+#30/09/14
+
+print("This program checks to see if a number you give is in a pre-set range.")
+number = float(input("Please enter your number: "))
+if 21<=number<=29 :
+    print("Your number is within the range.")
+else:
+    print("Your number is not in range.")
+    
